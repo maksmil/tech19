2dbf5a5260f52293330dd3936d573e49ed1a1b0b
20MM <114557868+maksmil@users.noreply.github.com>
2022-10-10 12:15:13 +0000

    karta pracy 3 zadanie

---
+++ KartaPracy3.py
@@ -0,0 +1,13 @@
+# a = int(input())
+# b = int(input())
+# if (1.3*a > b):
+#   print("TAK")
+# else:
+#   print("NIE")
+
+#zad1
+# n = int(input())
+# for i in range(n)
+#   print(i**3 + 3,end="")
+
+for i in range(15,32,2): print(i, end=" ")
