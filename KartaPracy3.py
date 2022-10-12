5229b7fe6763c11dfe0ee09581b48c2d1c0d842b
20MM <114557868+maksmil@users.noreply.github.com>
2022-10-12 05:50:29 +0000

    karta pracy 3

--- KartaPracy3.py
+++ KartaPracy3.py
@@ -1,13 +1,38 @@
-# a = int(input())
-# b = int(input())
-# if (1.3*a > b):
-#   print("TAK")
+# Ankieta
+# User wprowadza dwie liczby. 
+# Sprawdz czy druga jest 
+# o ponad 30% większa niż pierwsza
+# WE: p, q
+# WY: TAK/NIE
+
+# p, q = int(input()), int(input())
+# if (1.3*p > q):
+#     print("TAK")
 # else:
-#   print("NIE")
+#     print("NIE")
 
-#zad1
+# pętla liczb dwucyfrowych od 10 do 21
+
+# pętla liczb dwucyfrowych nieparzystych od 15 do 31  
+
+# pętla liczb dwucyfrowych malejąco (step ujemny)
+# for i in range(99,9,-1): print(i, end=" ")
+
+# pętla liczb dwucyfrowych malejąco (step dodatni)
+
+# pętla liczb 3-cyfrowych podzielnych przez 20
+
+#zad 2
+# for i in range(105,1000,15): print(i, end=" ")
+
+# #zad 3
 # n = int(input())
-# for i in range(n)
-#   print(i**3 + 3,end="")
+# for i in range(1,n+1):
+#   if n % i == 0:
+#     print(i, end=" ")
 
-for i in range(15,32,2): print(i, end=" ")
+#zad 4
+suma = 0
+for i in range(10,100):
+  suma = suma + 1
+  print(suma)
