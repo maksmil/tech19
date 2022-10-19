a06fd163fdff9885a18f723e7da648f2b3c9a2c8
20MM <114557868+maksmil@users.noreply.github.com>
2022-10-19 06:08:29 +0000

    wykonano kartapracy3a 2 zadania

---
+++ KartaPracy3a.py
@@ -0,0 +1,12 @@
+# n = int(input())
+# for i in range(n):
+#   print("*-|", end=" ")
+
+# #zad2
+# n= int(input())
+# for i in range(1, n+1):
+#   print("*"*i, end="")
+#   if i%2:
+#     print("||", end="")
+#   else:
+#     print("--", end="")
