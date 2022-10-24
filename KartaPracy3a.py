3efd4422b015da28460595b450a4d64e9082f73d
20MM <114557868+maksmil@users.noreply.github.com>
2022-10-24 12:16:08 +0000

    dodaje dalsze zadania kratypracy3a

--- KartaPracy3a.py
+++ KartaPracy3a.py
@@ -10,3 +10,16 @@
 #     print("||", end="")
 #   else:
 #     print("--", end="")
+# #zad 3
+# n = int(input())
+# for i in range(1, n+1):
+#   print("*", end="")
+#   if i % 2 ==1:
+#     print("|"*i, end="")
+#   else:
+#     print("-"*i, end="")
+
+for i in range(1,11):
+  for j in range(1,11):
+   print(i*j, end="\t")
+  print()
