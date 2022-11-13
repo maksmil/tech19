555ded0fbf7f0ab5f70dc94294ea8295029f0458
20MM <114557868+maksmil@users.noreply.github.com>
2022-11-13 13:11:10 +0000

    dodalem zadane zadana KartyPracy3b

--- KartaPracy3a.py
+++ KartaPracy3a.py
@@ -1,25 +1,73 @@
-# n = int(input())
-# for i in range(n):
-#   print("*-|", end=" ")
+#zad 1
+ n = int(input())
+ for i in range(1,32):
+   print(i)
 
-# #zad2
-# n= int(input())
-# for i in range(1, n+1):
-#   print("*"*i, end="")
-#   if i%2:
-#     print("||", end="")
-#   else:
-#     print("--", end="")
-# #zad 3
-# n = int(input())
-# for i in range(1, n+1):
-#   print("*", end="")
-#   if i % 2 ==1:
-#     print("|"*i, end="")
-#   else:
-#     print("-"*i, end="")
+#zad 2
+n = int(input())
+for i in range(1,n):
+    if i%2==1:
+        print(i**2, end=" ")
 
-for i in range(1,11):
-  for j in range(1,11):
-   print(i*j, end="\t")
-  print()
+#zad 3
+for i in range(1000,10000):
+    if i % 379 == 0:
+        print(i)
+
+#zad 4
+n = int(input())
+for i in range(100,1000):
+  if i % 5 ==0 or i % 6 ==0 or i % 11 ==0:
+    print(i)
+
+#zad 5
+suma = 0
+n = int(input())
+for i in range(0,n):
+  x = int(input())
+  suma+=x
+print(suma)
+
+#zad 6
+suma = 0
+k = int(input())
+for i in range(1,k):
+  if i%2 == 0:
+    suma+=i
+print(suma)
+
+#zad 7
+suma=0
+m = int(input())
+for i in range(1, m):
+    if i%2==1 and i>9 and 1<100:
+        suma=suma+i
+print(suma)
+
+
+#zad 8
+W0 = int(input())
+L = int(input())
+WX = 0
+suma = W0
+for i in range(0, L * 12):
+    WX = suma * 0.06 * (1/12)
+    suma = suma + WX
+print(round(suma))
+
+#zad 9
+from math import sqrt
+a = int(input())
+b = 21
+suma = 0 
+for i in range(0, a+1):
+    for o in range(0, i, b):
+        suma = suma + o
+        o = o + 100
+print(f"Suma to: {suma}")
+
+#zad 10
+from math import sqrt
+for i in range(1,1001):
+  if i%10 ==sqrt(i) or i%100 == sqrt(i) or i%1000 == sqrt(i):
+    print(i)
